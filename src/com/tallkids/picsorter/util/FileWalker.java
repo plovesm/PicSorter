8cf673afc56381dca8d4b4a62189878017952aea
plovesm <longfacepbo@gmail.com>
2014-05-06 08:40:31 -0500

    Update structure to FileWalker

--- src/com/tallkids/picsorter/util/FileWalker.java
+++ src/com/tallkids/picsorter/util/FileWalker.java
@@ -12,21 +12,22 @@ import java.io.File;
 public class FileWalker {
 	
 	private File activeSearchFile = new File("");
-	private String targetDir = "/Volumes/BlackBox2TB/Backups/Pictures/Photos_by_date";
+	private String targetDir = "";
+	private String searchDir = "";
 	private boolean match = false;
     private int missingFileCount = 0;
     
-    public void walkTarget( String path ) {
+    private void walkTarget(String path) {
 
-        File root = new File( path );
+        File root = new File(path);
         File[] list = root.listFiles();
 
         if (list == null) return;
         
-        for ( File f : list ) {
-            if ( f.isDirectory() ) {
-            	walkTarget( f.getAbsolutePath() );
-                //System.out.println( "Dir:" + f.getAbsoluteFile() );
+        for (File f : list) {
+            if (f.isDirectory()) {
+            	walkTarget(f.getAbsolutePath());
+                //System.out.println("Dir:" + f.getAbsoluteFile());
             }
             else {
             	if(f.equals(activeSearchFile))
@@ -39,39 +40,84 @@ public class FileWalker {
         
     }
     
-    public void walkSource( String path ) {
+    private void walkSource(String path) {
 
-        File root = new File( path );
+        File root = new File(path);
         File[] list = root.listFiles();
 
         if (list == null) return;
 
-        for ( File f : list ) {
-            if ( f.isDirectory() ) {
-                walkSource( f.getAbsolutePath() );
-                //System.out.println( "Dir:" + f.getAbsoluteFile() );
+        for (File f : list) {
+            if (f.isDirectory()) {
+                walkSource(f.getAbsolutePath());
+                //System.out.println("Dir:" + f.getAbsoluteFile());
             }
             else {
             	activeSearchFile = f;
             	
-            	//System.out.println( "Checking File: " + f.getName() );
+            	//System.out.println("Checking File: " + f.getName());
             	
             	walkTarget(targetDir);
             	
             	if(!match)
                 {
                 	setMissingFileCount(getMissingFileCount() + 1);
-            		System.out.println( f.getAbsolutePath() );
+            		System.out.println(f.getAbsolutePath());
                 }
             }
         }
     }
+    
     public int getMissingFileCount() {
 		return missingFileCount;
 	}
 
 	public void setMissingFileCount(int missingFileCount) {
 		this.missingFileCount = missingFileCount;
+	}
+	
+	/**
+	 * @return the targetDir
+	 */
+	public String getTargetDir() {
+		return targetDir;
+	}
+
+	/**
+	 * @param targetDir the targetDir to set
+	 */
+	public void setTargetDir(String targetDir) {
+		this.targetDir = targetDir;
+	}
+
+	/**
+	 * @return the searchDir
+	 */
+	public String getSearchDir() {
+		return searchDir;
+	}
+
+	/**
+	 * @param searchDir the searchDir to set
+	 */
+	public void setSearchDir(String searchDir) {
+		this.searchDir = searchDir;
+	}
+
+	public void searchDirectory(String searchDir, String targetDir)
+	{
+		String homeDir = System.getProperty("user.home");
+        
+		setTargetDir(homeDir + targetDir);
+		setSearchDir(homeDir + searchDir);
+		
+		checkIfFilesAreBackedUp();        
+		
+	}
+	
+	private void checkIfFilesAreBackedUp() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public static void main(String[] args) {
